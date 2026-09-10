2e8db50d8ddfad908ab6b0c1c7403e6ca3e8c041
Eimbiox <eimantas.fedenko04@gmail.com>
2026-09-10 15:35:02 +0200

    Added the logic for reading input about the specific good

--- Del-A.cs
+++ Del-A.cs
@@ -2,7 +2,8 @@ List<string> goods = ["Mjölk", "Smör"];
 List<int> prices = [15, 20];
 //Main Loop
 while (true)
-{   //Check if list is empty
+{   Console.Clear();
+    //Check if list is empty
     if (goods.Count() == 0)
     {
         Console.WriteLine("There is nothing in your basket");
@@ -17,6 +18,17 @@ while (true)
         int sum = prices.Sum();
         Console.WriteLine($"Total Price: {sum} kr");
     }
-    
+    //Ask for the item input and save in variable
+    Console.WriteLine("Type the name of the good you want to add: ");
+    string inputGood = Console.ReadLine().Trim();
+    //Check if item is with letters not numbers
+    if (int.TryParse(inputGood, out int temp))
+    {
+        Console.WriteLine("Please enter in a valid good");
+    }
+    else
+    {
+        Console.WriteLine($"Enter the price of {inputGood}");
+    }
     Console.ReadLine();
 }
