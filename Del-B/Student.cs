5f56e80ebaa2a490ea6a73b634a93070f3855a35
Eimbiox <eimantas.fedenko04@gmail.com>
2026-09-14 13:42:52 +0200

    har lagt till metoder i student klassen

--- Del-B/Student.cs
+++ Del-B/Student.cs
@@ -6,15 +6,43 @@ class Student(string name)
 
     public void Join(Course courseToJoin)
     {
-        
+        if (courseToJoin.Students.Count < courseToJoin.MaxSeats)
+        {
+            if (!Courses.Contains(courseToJoin))
+            {
+                Courses.Add(courseToJoin);
+                courseToJoin.Students.Add(this);
+            }
+            else
+            {
+                System.Console.WriteLine($"{this.Name} is already in {courseToJoin.Name} class");
+            }
+        }
+        else
+        {
+            System.Console.WriteLine($"{courseToJoin.Name} class is full");
+        }
     }
-    public void Leave()
+    public void Leave(Course courseToLeave)
     {
-        
+        if (courseToLeave.Students.Contains(this))
+        {
+            courseToLeave.Students.Remove(this);
+            Courses.Remove(courseToLeave);
+        }
+        else
+        {
+            System.Console.WriteLine($"{this.Name} is not in {courseToLeave.Name} class");
+        }
     }
     public void Schedule()
     {
-        
+        System.Console.WriteLine($"{this.Name} is in theese classes: ");
+
+        foreach (var course in Courses)
+        {
+            System.Console.WriteLine($"{course.Name} class");
+        }
     }
 
 }
