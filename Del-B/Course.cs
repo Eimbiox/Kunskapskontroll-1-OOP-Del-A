4c2c74ab5bbecc9e814afb59923947230788e049
Eimbiox <eimantas.fedenko04@gmail.com>
2026-09-14 12:11:03 +0200

    la till student och kurs class med construktor

---
+++ Del-B/Course.cs
@@ -0,0 +1,7 @@
+class Course(string name, int maxSeats)
+{
+    public string Name = name;
+    public int MaxSeats = maxSeats;
+
+    public List<Student> Students = [];
+}
---
+++ Del-B/Student.cs
@@ -0,0 +1,7 @@
+class Student(string name)
+{
+    public string Name = name;
+
+    public List<Course> Courses = [];
+
+}
